03c883217dd550a5bfa06d0c5747a2ba2cd94dfb
Katie Duane <katiejduane@gmail.com>
2019-06-03 21:47:58 -0400

    setting up bcrypt

--- back-end/models/user.js
+++ back-end/models/user.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../util/database');
 
+const bcrypt = require('bcrypt');
+// const saltRounds = 10;
+
 const User = sequelize.define('user', {
     id: {
         type: Sequelize.INTEGER,
@@ -10,11 +13,11 @@ const User = sequelize.define('user', {
     },
     firstname:{
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: true
     },
     lastname: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: true
     },
     email: {
         type: Sequelize.STRING,
@@ -22,8 +25,29 @@ const User = sequelize.define('user', {
     },
     hash: {
         type: Sequelize.STRING,
-        allowNull: true
+        allowNull: false
     }
 })
+
+User.beforeCreate((user, options) => {
+    return cryptPassword(user.hash)
+    .then(success => {
+        user.hash = success
+    })
+    .catch(err => console.log(err));
+});
+
+function cryptPassword(password){
+    console.log('crypt: ', password)
+    return new Promise((resolve, reject) => {
+        bcrypt.genSalt(10, (err, salt) => {
+            if(err) return reject(err);
+            bcrypt.hash(password, salt, null, (err, hash) => {
+                if(err) return reject(err);
+                return resolve(hash)
+            })
+        })
+    })
+}
 
 module.exports = User;
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const bcrypt = require('bcrypt');
+
 const userController = require('../controllers/user');
 
 router.get('/', userController.getAccount);
