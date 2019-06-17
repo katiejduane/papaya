9d48e383f97a21f05a54fabb149a9839c7636b66
Katie Duane <katiejduane@gmail.com>
2019-06-16 22:47:33 -0400

    no, papaya, i have not forgotten about you <3

--- front-end/src/store/actions/auth.js
+++ front-end/src/store/actions/auth.js
@@ -91,7 +91,7 @@ export const checkAuthTimeOut = (expirationTime) => {
     return dispatch => {
         setTimeout(() => {
             dispatch(signOut())
-        }, expirationTime);
+        }, expirationTime * 1000);
     }
 }
 
