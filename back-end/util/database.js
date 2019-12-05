a9c45d997c5209b230c3216263aa9dc20b68f475
Katie Duane <katiejduane@gmail.com>
2019-12-04 19:58:26 -0500

    db data changed

--- back-end/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('papaya', 'kathrynduane', 'papaya', {
-    dialect: 'postgresql',
-    host: 'localhost'
-})
-
-module.exports = sequelize;
